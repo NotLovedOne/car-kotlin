ff59dd9a6f47152b81858de0d83806fa6f45d44c
Yerassyl Oralgazin <fistofsnake@gmail.com>
2025-03-28 10:32:35 +0500

    init commit

---
+++ src/main/kotlin/com/example/carkotlin/CarKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.carkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/carkotlin/CarKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.carkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
